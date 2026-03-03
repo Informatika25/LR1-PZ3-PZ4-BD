af188646644883e1791bc904bcea81c605c00bde
anna-zolotova <anneta080307@gmail.com>
2026-03-03 15:46:05 +0300

    Add files via upload

--- SQL.py
+++ SQL.py
@@ -2,14 +2,9 @@ import mysql.connector
 from sqlalchemy import create_engine
 import pandas as pd
 
-engine = create_engine('mysql+mysqlconnector://j30084097_137:Gruppa137@srv221-h-st.jino.ru/j30084097_137')
 
-db_config = {
-        'user': 'j30084097_137',
-        'password': 'Gruppa137',
-        'host': 'srv221-h-st.jino.ru',
-        'database': 'j30084097_137'
-    }
+
+
 class Database():
     def __init__(self, table, db_config):
         self.db_config = db_config
@@ -72,6 +67,8 @@ class Database():
 
         res = self.cursor.fetchall()
         return pd.DataFrame(res)
+
+
     def print_for_id(self, table_name, firs_id, last_id):
         self.cursor.execute(f"SELECT * FROM {table_name} WHERE id")
         for i in range(firs_id, last_id):
@@ -123,24 +120,38 @@ class Database():
         df = pd.read_csv(file_name)
         df.to_sql(table_name, con=engine, if_exists='append', index=False)
 
+
+    def foreign_key(self, table_from, column, table_to):
+        self.cursor.execute(f"ALTER TABLE `{table_from}` ADD FOREIGN KEY (`{column}`) "
+                            f"REFERENCES `{table_to}`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;")
+        self.connection.commit()
+
+
 tab = Database("zolotova_fitnes_clubs", db_config)
-#print(tab.select_all())
+
+
+tab.foreign_key("zolotova_client_info", "client_id", "zolotova_fitnes_club_clients")
 # columns = {
-#     "name": "VARCHAR(100)",
-#     "age": "INT"
+#     "client_id" : "INT",
+#     "name" : "TEXT",
+#     "surname" : "TEXT",
+#     "age" : "INT"
 # }
-# tab.create("zolotova_test", columns)
+# tab.create("zolotova_client_info", columns)
 #tab.update(table_name="zolotova_fitnes_clubs", id="1", column="times_open", new_text="9:00-23:00")
-# tab.insert("zolotova_test",
-#            {'qwe': 4,
-#             'asd': 5,
-#            })
-#print(tab.select("zolotova_fitnes_club_clients",  "name", False))
+tab.insert("zolotova_client_info",
+            {'client_id': 3,
+            'name': "Екатерина",
+            'surname' : "Якушева",
+            'age' : 17
+            })
+#print(tab.select("zolotova_fitnes_club_clients",  "name", True))
 #tab.print_for_id("zolotova_fitnes_clubs", 0, 3)
-#tab.delete_for_id("zolotova_test", 9, 10)
+#tab.delete_for_id("zolotova_test", 2, 3)
 #print(tab.describe("zolotova_fitnes_clubs"))
 #print(tab.stroka_stolbec("zolotova_fitnes_club_coach", "Петр", "name"))
 #tab.delete_table("zolotova_test")
-#tab.add_column("zolotova_test", "asd", "VARCHAR(100)")
-#tab.improt_csv("zolotova_fitnes_clubsi", "zolotova_fitnes_clubs_file.csv")
-#tab.export_csv("zolotova_fitnes_clubs")
+# tab.add_column("zolotova_test", "zxc", "VARCHAR(100)")
+#tab.delete_column("zolotova_fitnes_club_clients", "age")
+# tab.export_csv("zolotova_fitnes_clubs")
+# tab.improt_csv("zolotova_import", "zolotova_fitnes_clubs_file.csv")
